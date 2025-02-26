04b6bdfb0654fe66991d7800996d23c341485882
Quicksc00per <104599739+Quicksc00per@users.noreply.github.com>
2025-02-26 13:39:35 -0500

    The base so far

---
+++ website personal/script.js
@@ -0,0 +1,11 @@
+document.getElementById("CS").addEventListener("click", function() {
+    new Audio("audio/sound-effects/happy-pop-1.mp3").play();
+    //Need to find a way where it plays a special animation of sort for picture
+    //Then needs to reverse the video as it is going back to the orginal
+});
+document.getElementById("GameDev").addEventListener("click", function() {
+    new Audio("audio/sound-effects/happy-pop-2.mp3").play();
+});
+document.getElementById("ITSpecialist").addEventListener("click", function() {
+    new Audio("audio/sound-effects/happy-pop-3.mp3").play();
+});
